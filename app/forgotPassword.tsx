422047bc73fdaf3cea254f34300b38e9e2ee6443
Breno <brenof8123@hotmail.com>
2024-08-05 11:10:59 -0300

    feat: Criando tela de "Esqueceu a senha?"

--- app/forgotPassword.tsx
+++ app/forgotPassword.tsx
@@ -27,10 +27,46 @@ import {
 import { Link } from "expo-router";
 import { AlertCircleIcon, CheckIcon } from "lucide-react-native";
 
-export default function SignIn() {
+export default function ForgotPassword() {
   return (
     <AppScreenTemplate screenDescription="Insira seu e-mail">
-      <Box></Box>
+      <Box>
+        <VStack space="4xl">
+          <FormControl size="lg" isInvalid={false} isRequired={true}>
+            <FormControlLabel mb="$2">
+              <FormControlLabelText>E-mail</FormControlLabelText>
+            </FormControlLabel>
+
+            <Input>
+              <InputField
+                type="text"
+                placeholder="E-mail"
+                placeholderTextColor="#DBDFE5"
+              />
+            </Input>
+
+            <FormControlHelper ml="$2">
+              <FormControlHelperText size="sm">
+                Insira seu e-mail para a recuração de senha.
+              </FormControlHelperText>
+            </FormControlHelper>
+
+            <FormControlError ml="$2">
+              <FormControlErrorIcon as={AlertCircleIcon} />
+
+              <FormControlErrorText>
+                At least 6 characters are required.
+              </FormControlErrorText>
+            </FormControlError>
+          </FormControl>
+
+          <Link href="/createPassword" asChild>
+            <Button size="lg" bgColor="#4C1D95">
+              <ButtonText fontWeight="$bold">Enviar link</ButtonText>
+            </Button>
+          </Link>
+        </VStack>
+      </Box>
     </AppScreenTemplate>
   );
 }
