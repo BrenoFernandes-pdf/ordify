b7e5cae4f618ca203b9bf009068a47ea2a35e4a3
Víctor Chagas <victorchagaslm@gmail.com>
2024-09-04 10:35:59 -0300

    feat: Implementando o modal de confirmação de exclusão

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -51,6 +51,7 @@ export default function Home() {
                           }}
                           alt={organizer.name}
                         />
+
                         <Text fontWeight="$bold" mt="$2" textAlign="center">
                           {organizer.name}
                         </Text>
---
+++ components/DeleteModal.tsx
@@ -0,0 +1,74 @@
+import {
+  Button,
+  ButtonText,
+  Heading,
+  Modal,
+  ModalBackdrop,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  Text,
+} from "@gluestack-ui/themed";
+import { X } from "lucide-react-native";
+
+type DeleteModalProps = {
+  name: string;
+  showDeleteModal: boolean;
+  setShowDeleteModal: (value: boolean) => void;
+  onDelete: () => void;
+};
+
+export default function DeleteModal({
+  name,
+  showDeleteModal,
+  setShowDeleteModal,
+  onDelete,
+}: DeleteModalProps) {
+  return (
+    <Modal
+      isOpen={showDeleteModal}
+      onClose={() => {
+        setShowDeleteModal(false);
+      }}
+    >
+      <ModalBackdrop />
+      <ModalContent>
+        <ModalHeader>
+          <Heading size="lg">Excluir {name}?</Heading>
+          <ModalCloseButton>
+            <X size={24} color="#FFFFFF" strokeWidth={2} />
+          </ModalCloseButton>
+        </ModalHeader>
+        <ModalBody>
+          <Text>Esta ação é irreversível.</Text>
+        </ModalBody>
+        <ModalFooter>
+          <Button
+            variant="outline"
+            size="sm"
+            action="secondary"
+            mr="$3"
+            onPress={() => {
+              setShowDeleteModal(false);
+            }}
+          >
+            <ButtonText>Cancelar</ButtonText>
+          </Button>
+          <Button
+            size="sm"
+            action="negative"
+            borderWidth="$0"
+            onPress={() => {
+              onDelete();
+              setShowDeleteModal(false);
+            }}
+          >
+            <ButtonText>Excluir</ButtonText>
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
